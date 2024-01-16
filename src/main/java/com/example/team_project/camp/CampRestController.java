7eaa79575c3d72503511f5371183310cb71d1f39
seunggod <kiss0703@nate.com>
2024-01-16 15:47:06 +0900

    order,campJWTOpen

--- src/main/java/com/example/team_project/camp/CampRestController.java
+++ src/main/java/com/example/team_project/camp/CampRestController.java
@@ -3,8 +3,6 @@ package com.example.team_project.camp;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,7 +22,6 @@ import com.example.team_project._core.utils.JwtTokenUtils;
 import com.example.team_project.camp._dto.CampReqDTO;
 import com.example.team_project.camp._dto.CampRespDTO;
 import com.example.team_project.camp._dto.CampRespDTO.CampDetailDTO;
-import com.example.team_project.camp._dto.CampRespDTO.CampListDTO;
 import com.example.team_project.camp.camp_bookmark.CampBookmark;
 
 import jakarta.validation.Valid;
@@ -49,10 +46,9 @@ public class CampRestController {
 
 
     // 캠핑장 상세정보 페이지
-     @GetMapping("/{id}")
-     public ResponseEntity<?> getCampDetail(@PathVariable Integer id) {
-         CampDetailDTO camp = campService.getCampDetail(id);
-
+     @GetMapping("/{campId}")
+     public ResponseEntity<?> getCampDetail(@PathVariable Integer campId) {
+         CampDetailDTO camp = campService.getCampDetail(campId);
          return ResponseEntity.ok(ApiUtils.success(camp));
      }
 
@@ -60,40 +56,41 @@ public class CampRestController {
     @PostMapping("/bookmark")
     public ResponseEntity<?> addBookmark(
             @Valid CampReqDTO.CampBookmarkDTO requestDTO
-    // , @RequestHeader("Authorization") String token
+     , @RequestHeader("Authorization") String token
     ) {
         // 토큰 처리를 위한 서비스를 통해 userId 추출
-        // DecodedJWT decodedJWT = JwtTokenUtils.verify(token);
-        // Integer userId = decodedJWT.getClaim("id").asInt();
+         DecodedJWT decodedJWT = JwtTokenUtils.verify(token);
+         Integer userId = decodedJWT.getClaim("id").asInt();
         System.out.println("컨트롤러");
-        campService.addBookmark(1, requestDTO);
+//        campService.addBookmark(1, requestDTO);
+        campService.addBookmark(userId, requestDTO);
 
-        // OrderRespDTO.myCampFieldListDTO responseDTO =
-        // orderService.myCampFieldList(userId, requestDTO);
+//         OrderRespDTO.myCampFieldListDTO responseDTO =
+//         orderService.myCampFieldList(userId, requestDTO);
         return ResponseEntity.ok(ApiUtils.success("북마크 성공"));
     }
 
     // 관심 캠핑장 등록 해제 기능
     @DeleteMapping("/bookmark")
     public ResponseEntity<?> removeBookmark(@Valid CampReqDTO.CampBookmarkDeleteDTO requestDTO
-           /*, @RequestHeader("Authorization") String token*/) {
-//        DecodedJWT decodedJWT = JwtTokenUtils.verify(token);
-//        Integer userId = decodedJWT.getClaim("id").asInt();
+           , @RequestHeader("Authorization") String token) {
+        DecodedJWT decodedJWT = JwtTokenUtils.verify(token);
+        Integer userId = decodedJWT.getClaim("id").asInt();
 
-        campService.removeBookmark(1, requestDTO);
-//        campService.removeBookmark(userId, requestDTO);
+//        campService.removeBookmark(1, requestDTO);
+        campService.removeBookmark(userId, requestDTO);
         return ResponseEntity.ok().body(ApiUtils.success("북마크 해제"));
     }
 
     // 관심 캠핑장 목록 조회 엔드포인트
     @GetMapping("/bookmarks")
-    public ResponseEntity<?> bookmarkList (/*@RequestHeader("Authorization") String token*/) {
-//      DecodedJWT decodedJWT = JwtTokenUtils.verify(token);
-//      Integer userId = decodedJWT.getClaim("id").asInt();    	
+    public ResponseEntity<?> bookmarkList (@RequestHeader("Authorization") String token) {
+      DecodedJWT decodedJWT = JwtTokenUtils.verify(token);
+      Integer userId = decodedJWT.getClaim("id").asInt();    	
     	
     	//테스트 용 하드 코딩
-        List<CampBookmark> responseDTO = campService.bookmarkList(1);
-//        List<CampBookmark> responseDTO = campService.bookmarkList(userId);
+//        List<CampBookmark> responseDTO = campService.bookmarkList(1);
+        List<CampBookmark> responseDTO = campService.bookmarkList(userId);
         return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
 
@@ -107,23 +104,22 @@ public class CampRestController {
     // ME 관심캠핑장 목록 페이지 요청
     // localhost:8080/camp/bookmark-list
     @GetMapping("/bookmark-list")
-    public ResponseEntity<?> campBookmarkPage() {
-        // @RequestHeader("Authorization") String token
-        // DecodedJWT decodedJWT = JwtTokenUtils.verify(token);
-        // Integer userId = decodedJWT.getClaim("id")
-        CampRespDTO.CampBookMarkListDTO responseDTO = campService.campBookMarkPage(1);
+    public ResponseEntity<?> campBookmarkPage(@RequestHeader("Authorization") String token) {
+         DecodedJWT decodedJWT = JwtTokenUtils.verify(token);
+         Integer userId = decodedJWT.getClaim("id").asInt();
+        CampRespDTO.CampBookMarkListDTO responseDTO = campService.campBookMarkPage(userId);
         return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
 
     // 내 캠핑장 연도별 목록 조회
     @GetMapping("/myCamp")
     public ResponseEntity<?> myCampList(@ModelAttribute CampReqDTO.MyCampListDTO requestDTO 
-    						/*,@RequestHeader("Authorization") String token*/){
-    	//DecodedJWT decodedJWT = JwtTokenUtils.verify(token);
-    	//Integer userId = decodedJWT.getClaim("id").asInt();
+    						,@RequestHeader("Authorization") String token){
+    	DecodedJWT decodedJWT = JwtTokenUtils.verify(token);
+    	Integer userId = decodedJWT.getClaim("id").asInt();
     	// 테스트 용 하드 코딩
-    	CampRespDTO.MyCampListDTO responseDTO = campService.myCampFieldList(1 , requestDTO);
-    	//OrderRespDTO.myCampFieldListDTO responseDTO = orderService.myCampFieldList(userId, requestDTO);
+//    	CampRespDTO.MyCampListDTO responseDTO = campService.myCampFieldList(1 , requestDTO);
+    	CampRespDTO.MyCampListDTO responseDTO = campService.myCampFieldList(userId, requestDTO);
     	return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
 
